a38c55250bde68f6ef2c7ebce39b5cdf92c5a7ab
TipTopMaestro <maestrotiptop@gmail.com>
2026-01-21 21:42:54 +0800

    google login

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+// import vue3GoogleLogin from 'vue3-google-login'
+import GoogleSignInPlugin from 'vue3-google-signin';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+let clientID = '189816827593-igu6ajdje3revunfdaphl0n8huv5fnti.apps.googleusercontent.com';
+app.use(GoogleSignInPlugin,
+    { 
+        clientId : clientID,
+    })
 app.mount('#app')
